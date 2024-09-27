7331ef16daa42a41e4d832b356408901698fab1a
maryprecious <maryprecious144@gmail.com>
2024-09-27 09:20:43 +0700

    rotate 2dimension matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,15 +4,30 @@
 
 
 def rotate_2d_matrix(matrix):
-        """ Given an n x n 2D matrix, rotate it 90 degrees clockwise
-            """
-                for x, y in enumerate(zip(*reversed(matrix))):
-                            matrix[x] = list(y)
+    """
+        Rotates a 2D matrix 90 degrees clockwise in place.
+            
+        Args:
+        matrix (list of list of int): The 2D matrix to rotate.
+                        
+        Do not return anything. The matrix is modified in place.
+    """
+    n = len(matrix)
+
+        # Transpose the matrix
+            for i in range(n):
+                        for j in range(i, n):
+                                        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+                                            # Reverse each row
+                                                for i in range(n):
+                                                            matrix[i].reverse()
 
 
-                            if __name__ == '__main__':
-                                    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+                                                            if __name__ == "__main__":
+                                                                    matrix = [[1, 2, 3],
+                                                                                          [4, 5, 6],
+                                                                                                        [7, 8, 9]]
 
-                                        """ rotate_2d_matrix(matrix) """
-                                            rotate_2d_matrix(matrix)
-                                                print(matrix)
+                                                                        rotate_2d_matrix(matrix)
+                                                                            print(matrix)
